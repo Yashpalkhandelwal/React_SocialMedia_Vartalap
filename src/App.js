96049e38b7708d4c7fdacf3390d47a63b01e7709
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2020-07-27 16:38:27 +0530

    singup and login done and post can be added

--- src/App.js
+++ src/App.js
@@ -1,77 +1,213 @@
-import React, {useState, useEffect} from 'react';
-import Post from "./Components/Post";
-import {db} from "./firebase.js";
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import {Button} from "@material-ui/core";
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
+import Post from "./Post";
+import {db, auth} from "./firebase";
+import {makeStyles} from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import {Button, Input} from "@material-ui/core";
+import ImageUpload from "./ImageUpload";
+import "./plus.png";
 
 function getModalStyle() {
     const top = 50;
     const left = 50;
 
-    return {
-      top: `${top}%`, 
-      left: `${left}%`, 
-      transform: `translate(-${top}%, -${left}%)`};
+    return {top: `${top}%`, left: `${left}%`, transform: `translate(-${top}%, -${left}%)`};
 }
 
 const useStyles = makeStyles((theme) => ({
     paper: {
+        position: "absolute",
         width: 400,
         backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
+        border: "2px solid #000",
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3)
     }
 }));
 
 function App() {
-   const classes = useStyles();
-   const [modalStyle] = useState(getModalStyle);
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
     const [posts, setPosts] = useState([]);
     const [open, setOpen] = useState(false);
+    const [openSignIn, setOpenSignIn] = useState(false);
+    const [openaddpost, setOpenAddPost] = useState(false);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                //user has logged in....
+                setUser(authUser);
+            } else {
+                //user has logged out
+                setUser(null);
+            }
+        });
+        return() => {
+            unsubscribe();
+        };
+    }, [user, username]);
 
     useEffect(() => {
         db
-            .collection('posts')
-            .onSnapshot(snapshort => {
-                setPosts(snapshort.docs.map(doc => ({id: doc.id, post: doc.data()})));
+            .collection("posts")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) => {
+                setPosts(snapshot.docs.map((doc) => ({id: doc.id, post: doc.data()})));
+            });
+    }, []);
+
+    const signUp = (event) => {
+        event.preventDefault();
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((authUser) => {
+                let userInfo = authUser
+                    .user
+                    .updateProfile({displayName: username});
+                setUser({displayName: username});
+                return userInfo;
             })
-    }, [])
+            .catch((error) => alert(error.message));
+        setOpen(false);
+    };
 
-    const singup = (event) => {
+    const signIn = (event) => {
+        event.preventDefault();
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .catch((error) => alert(error.message));
 
-    }
+        setOpenSignIn(false);
+    };
 
     return (
         <div className="app">
-            <Modal
-                open={open}
-                onClose={() => setOpen(false)}
-               >
+            <Modal open={open} onClose={() => setOpen(false)}>
                 <div style={modalStyle} className={classes.paper}>
-                    <h2>I AM A  modal</h2>
-               </div>
+                    <form className="app__signup">
+                        <center>
+                            <h1 className="app_logo">
+                                Vartalapp
+                            </h1>
+                        </center>
+                        <Input
+                            type="text"
+                            placeholder="UserName"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}/>
+                        <Input
+                            type="text"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}/>
+                        <Input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+
+                        <Button type="submit" onClick={signUp}>
+                            Sign Up
+                        </Button>
+                    </form>
+                </div>
             </Modal>
 
-            <div className="app_header">
-                <h1 className="app_logo" alt="logo">
+            <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+                <div style={modalStyle} className={classes.paper}>
+                    <form className="app__signup">
+                        <center>
+                            <h1 className="app_logo">
+                                Vartalapp
+                            </h1>
+                        </center>
+                        <Input
+                            type="text"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}/>
+                        <Input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+
+                        <Button type="submit" onClick={signIn}>
+                            Sign In
+                        </Button>
+                    </form>
+                </div>
+            </Modal>
+
+            <div className="app__header">
+                <h1 className="app_logo">
                     Vartalapp
                 </h1>
+                {
+                    user
+                        ? (
+                            <div className="app__logoutContainer">
+                                <Button onClick={() => auth.signOut()}>Logout</Button>
+                            </div>
+                            )
+                        : (
+                            <div className="app__loginContainer">
+                                <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+                                <Button onClick={() => setOpen(true)}>Sign Up</Button>
+                            </div>
+                        )
+                }
             </div>
-            <Button onClick={() => setOpen(true)} >
-                Sign Up
-            </Button>
             {
-                posts.map(({id, post}) => (
-                    <Post
-                        key={id}
-                        imgURL={post.imgURL}
-                        username={post.username}
-                        caption={post.caption}/>
-                ))
+                user
+                    ?.displayName
+                        ? (
+                            <div>
+                                <div className="app__posts">
+                                    {
+                                        posts.map(({id, post}) => (
+                                            <Post
+                                                key={id}
+                                                postId={id}
+                                                imageUrl={post.imageUrl}
+                                                username={post.username}
+                                                caption={post.caption}
+                                                user={user}/>
+                                        ))
+                                    }
+                                </div>
+                                <div className="app_bottom">
+                                  <Button className="app_addpostbutton" onClick={() => setOpenAddPost(true)}><img className="app_addposticon" src="https://image.flaticon.com/icons/svg/1004/1004759.svg"  alt="Add Post"/>Add post</Button>                                   
+                                </div>
+                            </div>
+
+                        )
+                        : (
+                            <h3 className="app__loginmessage">
+                                Sorry! you need to login to upload
+                            </h3>
+                        )
             }
+            <Modal open={openaddpost} onClose={() => setOpenAddPost(false)}>
+                <div style={modalStyle} className={classes.paper}>
+                    <form className="app__signup">
+                    {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3 className="app__loginmessage">
+          Sorry! Something went wrong...
+        </h3>
+      )}
+                    </form>
+                </div>
+            </Modal>
+
         </div>
     );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,18 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-
-    apiKey: "AIzaSyC3pSi_aA0AvQ6Fbm8qWcrJPKmLagdL_dE",
-    authDomain: "vartalaap-46761.firebaseapp.com",
-    databaseURL: "https://vartalaap-46761.firebaseio.com",
-    projectId: "vartalaap-46761",
-    storageBucket: "vartalaap-46761.appspot.com",
-    messagingSenderId: "398678959334",
-    appId: "1:398678959334:web:3bf2a26d9c6704698b4727",
-    measurementId: "G-4S8D0CV6M9"
-
+  apiKey: "AIzaSyAt9ZJ4yCmU6qxwz8FIxshoDe-2wocaVq0",
+  authDomain: "instagram-clone-2e5d0.firebaseapp.com",
+  databaseURL: "https://instagram-clone-2e5d0.firebaseio.com",
+  projectId: "instagram-clone-2e5d0",
+  storageBucket: "instagram-clone-2e5d0.appspot.com",
+  messagingSenderId: "353063610956",
+  appId: "1:353063610956:web:270805ab42ec3b58c862e9",
+  measurementId: "G-Y9VGZ07J9M",
 });
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
-export {
-    db,
-    auth,
-    storage
-};
+export { db, auth, storage };
